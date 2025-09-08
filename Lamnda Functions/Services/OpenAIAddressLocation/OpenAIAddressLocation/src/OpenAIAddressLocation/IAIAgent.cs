1bcf612f6a2b8619487089cdbed3efd9cc55b909
mosmatan <matanmo@mta.ac.il>
2025-09-08 17:03:48 +0300

    remove unused code files

--- Lamnda Functions/Services/OpenAIAddressLocation/OpenAIAddressLocation/src/OpenAIAddressLocation/IAIAgent.cs
+++
@@ -1,9 +0,0 @@
-using Amazon.Lambda.Core;
-
-namespace OpenAIAddressLocation;
-
-public interface IAIAgent
-{
-    ILambdaLogger? Logger { get; set; }
-    Task<Coordinates> GetCoordinatesAsync(string address);
-}
